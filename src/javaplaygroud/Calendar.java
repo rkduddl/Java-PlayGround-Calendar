25d61a5b6d77ddb6775dc3f0d0ad0d5d29455d82
dlrkdud <rkduddl030@naver.com>
2022-09-23 15:10:44 +0900

    month

--- src/javaplaygroud/Calendar.java
+++ src/javaplaygroud/Calendar.java
@@ -1,14 +1,28 @@
 package javaplaygroud;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
-		System.out.println(" ì¼ ì›” í™” ìˆ˜ ëª© ê¸ˆ í† ");
+		System.out.println(" ÀÏ ¿ù È­ ¼ö ¸ñ ±İ Åä");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7 ");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		
+		
+		
+		//¼ıÀÚ¸¦ ÀÔ·Â¹Ş¾Æ ÇØ´çÇÏ´Â ´ŞÀÇ ÃÖ´ë ÀÏ¼ö¸¦ Ãâ·ÂÇÏ´Â ÇÁ·Î±×·¥
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("´ŞÀ» ÀÔ·ÂÇÏ¼¼¿ä.");
+		int month = scanner.nextInt();
+		
+		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		System.out.printf("%d¿ùÀº %dÀÏ±îÁö ÀÖ½À´Ï´Ù.\n", month, maxDays[month-1]);
+		scanner.close();
 
 	}
 
