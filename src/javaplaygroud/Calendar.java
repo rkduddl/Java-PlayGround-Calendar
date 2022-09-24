dde3c6c0b51f92e84fc9bea8dc4db2053ae40856
dlrkdud <rkduddl030@naver.com>
2022-09-24 17:17:31 +0900

    Leap weekday

--- src/javaplaygroud/Calendar.java
+++ src/javaplaygroud/Calendar.java
@@ -1,7 +1,5 @@
 package javaplaygroud;
 
-import java.util.Scanner;
-
 public class Calendar {
 
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
@@ -23,24 +21,41 @@ public class Calendar {
 		}
 	}
 
-	public void printCalendar(int year, int month) {
+	public void printCalendar(int year, int month, int weekday) {
 		System.out.printf("   <<%4d년 %3d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
 
-		int maxDay = getmaxDaysOfmonth(year, month);
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+		}
 
-		for (int i = 1; i <= maxDay; i++) {
+		int maxDay = getmaxDaysOfmonth(year, month);
+		int count = 7 - weekday;
+		int delim = (count < 7) ? count : 0;
+		
+		
+		/*
+		if (count < 7) {
+			delim = count;
+		} else {
+			delim = 0;
+		} */
+
+		for (int i = 1; i <= count; i++) {
 			System.out.printf("%3d", i);
-			if (i % 7 == 0)
+		}
+		System.out.println();
+
+		count++;
+		for (int i = count; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == delim)
 				System.out.println();
 		}
 
 		System.out.println();
-//		System.out.println(" 1  2  3  4  5  6  7 ");
-//		System.out.println(" 8  9 10 11 12 13 14");
-//		System.out.println("15 16 17 18 19 20 21");
-//		System.out.println("22 23 24 25 26 27 28");
+		System.out.println();
 	}
 
 }
