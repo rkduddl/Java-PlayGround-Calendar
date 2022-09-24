fd0faecc0100d6df4e939bbc3db6fa8722ea38ff
dlrkdud <rkduddl030@naver.com>
2022-09-24 16:05:13 +0900

    Leap Max Day

---
+++ src/javaplaygroud/Prompt.java
@@ -0,0 +1,47 @@
+package javaplaygroud;
+
+import java.util.Scanner;
+
+public class Prompt {
+
+	private final static String PROMPT = "cal> ";
+
+	public void runPrompt() {
+		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
+
+		int month = 1;
+		int year = -1;
+
+		while (true) {
+			System.out.println("년도를 입력하세요.");
+			System.out.print("YEAR> ");
+			year = scanner.nextInt();
+			System.out.println("달을 입력하세요.");
+			System.out.print("MONTH> ");
+			month = scanner.nextInt();
+			
+			if (month == -1) {
+				break;
+			}
+
+			if (month > 12) {
+				continue;
+			}
+
+			cal.printCalendar(year, month);
+		}
+
+		System.out.println("끝!");
+		scanner.close();
+
+	}
+
+	public static void main(String[] args) {
+		// 셀 싱행
+		Prompt p = new Prompt();
+		p.runPrompt();
+
+	}
+
+}
